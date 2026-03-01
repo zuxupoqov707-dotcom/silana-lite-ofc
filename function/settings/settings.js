8dd979773c81b9270ffdbe20c29440c76c4122b2
zuxupoqov707-dotcom <zuxupoqov707@gmail.com>
2026-03-01 19:58:39 +0000

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,18 +14,18 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '4915510394223',
+ pairingNumber : '4915510394223',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
+ nomorwa : '212701810216',
  nameown : 'NOUREDDINE',
  nomerown : '212717457920',
  packname : 'sticker by ',
  author : 'SILANA AI',
- namebot : '乂 SILANA AI',
- wm : 'SILANA LITE.',
+ namebot : '乂 𝕝𝕠𝕚𝕕𝕖 𝕗𝕣𝕠𝕥𝕚𝕣𝕒 AI',
+ wm : '𝕝𝕠𝕚𝕕𝕖 𝕗𝕣𝕠𝕥𝕚𝕣𝕒 LITE.',
  stickpack : 'Whatsapp',
- stickauth : 'SILANA LITE',
+ stickauth : '𝕝𝕠𝕚𝕕𝕖 𝕗𝕣𝕠𝕥𝕚𝕣𝕒 LITE',
  jid: '@s.whatsapp.net'
 }
 
@@ -35,8 +35,8 @@ global.media = {
  didyou : 'https://telegra.ph/file/e40219cc8d472025f18b6.jpg',
  rulesBot : 'https://telegra.ph/file/7cd8985216ffd3c376238.jpg',
  thumbnail : 'https://files.catbox.moe/hnbuh3.jpg',
- thumb : 'https://files.catbox.moe/hnbuh3.jpg',
- logo : 'https://files.catbox.moe/hnbuh3.jpg',
+ thumb : 'https://files.catbox.moe/c860gi.jpg',
+ logo : 'https://files.catbox.moe/c860gi.jpg',
  unReg : 'https://telegra.ph/file/ef02d1fdd59082d05f08d.jpg',
  registrasi : 'https://files.catbox.moe/hnbuh3.jpg',
  confess : 'https://telegra.ph/file/bfef7e33bc60dd2608ac6.jpg',
